a0c98a0b795776cfb312497294d03dacfbbb7e60
James Moore <jameslmoore@gmail.com>
2017-06-09 17:43:57 -1000

    got image urls

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
 console.log("works");
+
+//https://www.reddit.com/r/EarthPorn.json
+
+function reqListener () {
+  //console.log(this.responseText);
+  var response = JSON.parse(this.responseText);
+  // console.log(response.data.children[0].data);
+  // console.log(response.data.children[2].data);
+  // console.log(response.data.children[3].data.url);
+
+
+  for (var i = 0; i < 4; i++){
+    if (response.data.children[i].kind === "t3"){
+      console.log("match",response.data.children[i].kind);
+      if (response.data.children[i].data.post_hint === "image"){
+        console.log(response.data.children[i].data.url);
+      }
+    }
+  }
+}
+
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", reqListener);
+oReq.open("GET", "https://www.reddit.com/r/EarthPorn.json");
+oReq.send();
